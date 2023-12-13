f03f08a0b6b11d353ababded911946dd4e41c6df
Urushira <Listopadvadim@yandex.ru>
2023-12-13 15:14:11 +0300

    Создал класс и закомментил для doxygen

---
+++ Listopad_Person/Listopad_description.h
@@ -0,0 +1,13 @@
+/**
+	@mainpage Проект Listopad_Train для тестирования Doxygen
+	Используется в лабораторной работе номер 3
+	по курсу Проектирование программного обеспечения
+	@copyright ВоГУ
+	@author Листопад В.Ю.
+	@date 13-12-2023
+\par Использует класс:
+- @ref Listopad_Train
+\par Содержит файлы:
+- @ref Listopad_Train.h
+- @ref Listopad_Train.cpp
+*/
