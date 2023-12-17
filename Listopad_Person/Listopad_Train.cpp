6a8bebef9f349e917f7d3b33fba5f31b60872dbf
Urushira <Listopadvadim@yandex.ru>
2023-12-17 23:36:52 +0300

    Перевыгрузка

---
+++ Listopad_Person/Listopad_Train.cpp
@@ -0,0 +1,21 @@
+#include "Listopad_Train.h"
+/**
+	@file		Listopad_Train.cpp
+	@brief		Файл исходных кодов класса поезда
+	@copyright	ВоГУ
+	@author		Листопад В.Ю.
+	@date		13-12-2023
+	@version	0.3.1
+\par Использует файл:
+	@ref Listopad_Train.h
+\par Содержит класс:
+	@ref Listopad_Train
+*/
+
+Listopad_Train::Listopad_Train() {
+
+}
+
+Listopad_Train::~Listopad_Train() {
+		
+}
